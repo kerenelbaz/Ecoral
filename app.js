61a7eb90ccdfe6376dc5ffec3223352cdbb48d2e
lirazicoh <lirazicoh@gmail.com>
2024-02-18 21:59:46 +0200

    name

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
-const express = require('express');
-const morgan = require('morgan');
+const express = require("express");
+const morgan = require("morgan");
 
-const diveRouter = require('./routes/diveRoutes');
+const diveRouter = require("./routes/diveRoutes");
 
 const app = express();
 
 //1. midlewares
-if (process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'));
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
 }
 
 app.use(express.json());
 //app.use(express.static(`${__dirname}/public`));
 
-app.use('/api/dives', diveRouter);
+app.use("/api/dives", diveRouter);
 
 module.exports = app;
 //lizi
+
+// amit
