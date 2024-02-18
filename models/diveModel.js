6bdca2773879a9dcde6a281eb00ecd3048a881a6
kerenelbaz <132551849+kerenelbaz@users.noreply.github.com>
2024-02-18 11:25:06 +0200

    Update diveModel.js

--- models/diveModel.js
+++ models/diveModel.js
@@ -8,124 +8,124 @@ const diveSchema = new mongoose.Schema({
         trim: true,
 
     },
-    // objectCode:{
-    //     type: String,
-    //     trim: true,
+    objectCode: {
+        type: String,
+        trim: true,
 
-    // },
-    // date:{
-    //     type: Date,
-
-
-    // },
-    // time:{
-    //     type: String,
-
-    // },
-    // diveSite:{
-    //     type: String,
-
-    // },
-    // objectGroup:{
-    //     type: String,
-
-    // },
-    // specie:{
-    //     type: String,
-
-    // },
-    // idCode_photographerName:{
-    //     type: String,
-
-    // },
-    // imageLocation:{
-    //     type: String,
-
-    // },
-    // AR:{
-    //     type: Boolean,
+    },
+    date: {
+        type: Date,
 
 
-    // },
-    // humanWildlifeInteraction:{
-    //     type: String,
+    },
+    time: {
+        type: String,
 
-    // },
-    // reportType:{
-    //     type: String,
-    //     enum: {
-    //         values: ['snorkeling', 'SCUBA', 'freediving'],
-    //         message: 'reportType is either: snorkeling, SCUBA freediving'
-    //     },
+    },
+    diveSite: {
+        type: String,
 
-    // },
-    // typeOfDive:{
-    //     type: String,
+    },
+    objectGroup: {
+        type: String,
 
-    // },
-    // rankOfDive:{
-    //     type: Number,
-    //     enum: {
-    //         values: [1,2,3,4,5],
-    //         message: 'rankOfDive is between 1-5',
-    //     },
+    },
+    specie: {
+        type: String,
 
-    // },
-    // media:{
-    //     type: String,
+    },
+    idCode_photographerName: {
+        type: String,
 
-    // },    
-    // documentation:{
-    //     type: String,
+    },
+    imageLocation: {
+        type: String,
 
-    // },
-    // ageOfDiver:{
-    //     type: String,
+    },
+    AR: {
+        type: Boolean,
 
 
-    // },
-    // sexOfDiver:{
-    //     type: String,
+    },
+    humanWildlifeInteraction: {
+        type: String,
 
-    // },
-    // maxDepth:{
-    //     type: Number,
+    },
+    reportType: {
+        type: String,
+        enum: {
+            values: ['snorkeling', 'SCUBA', 'freediving'],
+            message: 'reportType is either: snorkeling, SCUBA freediving'
+        },
+
+    },
+    typeOfDive: {
+        type: String,
+
+    },
+    rankOfDive: {
+        type: Number,
+        enum: {
+            values: [1, 2, 3, 4, 5],
+            message: 'rankOfDive is between 1-5',
+        },
+
+    },
+    media: {
+        type: String,
+
+    },
+    documentation: {
+        type: String,
+
+    },
+    ageOfDiver: {
+        type: String,
 
 
-    // },
-    // distance:{
-    //     type: Number,
+    },
+    sexOfDiver: {
+        type: String,
 
-    // },
-    // temp:{
-    //     type: Number,
+    },
+    maxDepth: {
+        type: Number,
 
-    // },
-    // userDescription:{
-    //     type: String,
-    //     trim: true,
 
-    // },
-    // researcherComment:{
-    //     type: String,
-    //     trim: true,
-    // },
-    // linkURL:{
-    //     type: String,
+    },
+    distance: {
+        type: Number,
 
-    // },
-    // loggedBy:{
-    //     type: String,
+    },
+    temp: {
+        type: Number,
 
-    // },
-    // loggingDate:{
-    //     type: Date,
-    //     default:Date.now(),
+    },
+    userDescription: {
+        type: String,
+        trim: true,
 
-    // },
-    // reportReceivingDate:{
-    //     type: Date,
-    // }
+    },
+    researcherComment: {
+        type: String,
+        trim: true,
+    },
+    linkURL: {
+        type: String,
+
+    },
+    loggedBy: {
+        type: String,
+
+    },
+    loggingDate: {
+        type: Date,
+        default: Date.now(),
+
+    },
+    reportReceivingDate: {
+        type: Date,
+    }
 
 });
 
