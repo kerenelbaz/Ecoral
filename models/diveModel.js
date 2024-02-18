199d42dd2351b3eff1044d0c2fc76de08b054514
kerenelbaz <132551849+kerenelbaz@users.noreply.github.com>
2024-02-18 11:08:14 +0200

    fixed getAllDives

--- models/diveModel.js
+++ models/diveModel.js
@@ -1,134 +1,135 @@
+const { Int32 } = require('mongodb');
 const mongoose = require('mongoose');
 
 const diveSchema = new mongoose.Schema({
-    diveCode:{
-        type: String,
+    diveCode: {
+        type: Number,
         unique: true,
         trim: true,
-        
-    },
-    objectCode:{
-        type: String,
-        trim: true,
 
     },
-    date:{
-        type: Date,
-        
+    // objectCode:{
+    //     type: String,
+    //     trim: true,
 
-    },
-    time:{
-        type: String,
-
-    },
-    diveSite:{
-        type: String,
-
-    },
-    objectGroup:{
-        type: String,
-
-    },
-    specie:{
-        type: String,
-
-    },
-    idCode_photographerName:{
-        type: String,
-
-    },
-    imageLocation:{
-        type: String,
-
-    },
-    AR:{
-        type: Boolean,
-        
-
-    },
-    humanWildlifeInteraction:{
-        type: String,
-
-    },
-    reportType:{
-        type: String,
-        enum: {
-            values: ['snorkeling', 'SCUBA', 'freediving'],
-            message: 'reportType is either: snorkeling, SCUBA freediving'
-        },
-
-    },
-    typeOfDive:{
-        type: String,
-
-    },
-    rankOfDive:{
-        type: Number,
-        enum: {
-            values: [1,2,3,4,5],
-            message: 'rankOfDive is between 1-5',
-        },
-
-    },
-    media:{
-        type: String,
-
-    },    
-    documentation:{
-        type: String,
-
-    },
-    ageOfDiver:{
-        type: String,
-
-        
-    },
-    sexOfDiver:{
-        type: String,
-
-    },
-    maxDepth:{
-        type: Number,
+    // },
+    // date:{
+    //     type: Date,
 
 
-    },
-    distance:{
-        type: Number,
+    // },
+    // time:{
+    //     type: String,
 
-    },
-    temp:{
-        type: Number,
+    // },
+    // diveSite:{
+    //     type: String,
 
-    },
-    userDescription:{
-        type: String,
-        trim: true,
+    // },
+    // objectGroup:{
+    //     type: String,
 
-    },
-    researcherComment:{
-        type: String,
-        trim: true,
-    },
-    linkURL:{
-        type: String,
+    // },
+    // specie:{
+    //     type: String,
 
-    },
-    loggedBy:{
-        type: String,
+    // },
+    // idCode_photographerName:{
+    //     type: String,
 
-    },
-    loggingDate:{
-        type: Date,
-        default:Date.now(),
+    // },
+    // imageLocation:{
+    //     type: String,
 
-    },
-    reportReceivingDate:{
-        type: Date,
-    }
+    // },
+    // AR:{
+    //     type: Boolean,
+
+
+    // },
+    // humanWildlifeInteraction:{
+    //     type: String,
+
+    // },
+    // reportType:{
+    //     type: String,
+    //     enum: {
+    //         values: ['snorkeling', 'SCUBA', 'freediving'],
+    //         message: 'reportType is either: snorkeling, SCUBA freediving'
+    //     },
+
+    // },
+    // typeOfDive:{
+    //     type: String,
+
+    // },
+    // rankOfDive:{
+    //     type: Number,
+    //     enum: {
+    //         values: [1,2,3,4,5],
+    //         message: 'rankOfDive is between 1-5',
+    //     },
+
+    // },
+    // media:{
+    //     type: String,
+
+    // },    
+    // documentation:{
+    //     type: String,
+
+    // },
+    // ageOfDiver:{
+    //     type: String,
+
+
+    // },
+    // sexOfDiver:{
+    //     type: String,
+
+    // },
+    // maxDepth:{
+    //     type: Number,
+
+
+    // },
+    // distance:{
+    //     type: Number,
+
+    // },
+    // temp:{
+    //     type: Number,
+
+    // },
+    // userDescription:{
+    //     type: String,
+    //     trim: true,
+
+    // },
+    // researcherComment:{
+    //     type: String,
+    //     trim: true,
+    // },
+    // linkURL:{
+    //     type: String,
+
+    // },
+    // loggedBy:{
+    //     type: String,
+
+    // },
+    // loggingDate:{
+    //     type: Date,
+    //     default:Date.now(),
+
+    // },
+    // reportReceivingDate:{
+    //     type: Date,
+    // }
 
 });
 
 
-const Dive = mongoose.model('Dive' , diveSchema);
+const Dive = mongoose.model('dives', diveSchema);
 
-module.exports= Dive;
+module.exports = Dive;
