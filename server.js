5a81aa4a8e735d8a3032a13e7fd5d2f3ce4336ac
lirazicoh <lirazicoh@gmail.com>
2024-04-17 15:27:46 +0300

    background color changed by lizi

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ mongoose
     });
 
 //start the server
-const port = process.env.PORT || 3000;
+const port = 3030||process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`App is running on port ${port}`);
 });
