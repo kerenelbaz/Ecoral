124134d1d8f5da2ec4f4948a0e7915ac195fc4c6
lirazicoh <lirazicoh@gmail.com>
2024-04-25 14:57:12 +0300

    cores added

--- server.js
+++ server.js
@@ -1,27 +1,29 @@
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const cors = require("cors"); // Import CORS middleware
 
 dotenv.config({ path: "./config.env" });
 const app = require("./app");
 
 const DB = process.env.DATABASE.replace(
-    "<PASSWORD>",
-    process.env.DATABASE_PASSWORD
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD
 );
 
 mongoose
-    .connect(DB, {
-        useNewUrlParser: true,
-        //useCreateIndex: true,
-        //useFindAndModify: false,
-        useUnifiedTopology: true,
-    })
-    .then(() => {
-        console.log("db connected!!!");
-    });
+  .connect(DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("db connected!!!");
+  });
+
+// Enable CORS middleware
+app.use(cors());
 
 //start the server
-const port = 3030||process.env.PORT || 3000;
+const port = 3030 || process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log(`App is running on port ${port}`);
+  console.log(`App is running on port ${port}`);
 });
